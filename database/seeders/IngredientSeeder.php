03571aee6e6d285c5baafb98f4fddefbf5472a46
Justin <justinpoot@live.nl>
2021-03-07 12:01:18 +0100

    Use ingredient factory in ingredient seeder.

--- database/seeders/IngredientSeeder.php
+++ database/seeders/IngredientSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Ingredient;
 use Illuminate\Support\Str;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
@@ -15,8 +16,8 @@ class IngredientSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('ingredients')->insert([
-            'name' => Str::random(10),
-        ]);
+        Ingredient::factory()
+            ->count(20)
+            ->create();
     }
 }
