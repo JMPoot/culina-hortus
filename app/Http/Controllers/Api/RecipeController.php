9a900a4aaa0760625bd4d30dd2218fbad00d25f4
Justin <justinpoot@live.nl>
2021-03-10 21:30:50 +0100

    Add responses to recipe controller.

--- app/Http/Controllers/Api/RecipeController.php
+++ app/Http/Controllers/Api/RecipeController.php
@@ -6,31 +6,41 @@ use App\Models\Recipe;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use App\Http\Controllers\Controller;
+use App\Http\Resources\RecipeCollection;
+use App\Http\Resources\RecipeResource;
 
 class RecipeController extends Controller
 {
     public function index() 
     {
-        return response(null, Response::HTTP_NOT_IMPLEMENTED);
+        return (new RecipeCollection(Recipe::all()))->response();
     }
 
     public function store(Request $request) 
     {
-        return response(null, Response::HTTP_NOT_IMPLEMENTED);
+        $this->validate($request, [
+            'title' => 'required',
+            'description' => 'required'
+        ]);
+        
+        $recipe = Recipe::create($request->all());
+        return (new RecipeResource($recipe))->response(Response::HTTP_CREATED);
     }
 
     public function show(Recipe $recipe)
     {
-        return response(null, Response::HTTP_NOT_IMPLEMENTED);
+        return (new RecipeResource($recipe))->response();
     }
 
     public function update(Request $request, Recipe $recipe)
     {
-        return response(null, Response::HTTP_NOT_IMPLEMENTED);
+        $recipe->update($request->all());
+        return (new RecipeResource($recipe))->response();
     }
 
     public function destory(Recipe $recipe) 
     {
-        return response(null, Response::HTTP_NOT_IMPLEMENTED);
+        $recipe->delete();
+        return response(null, Response::HTTP_NO_CONTENT);
     }
 }
